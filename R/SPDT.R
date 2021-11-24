c42d3bc92bd9fcd10f601e035f902fc8e947fcc2
Askey <Paul.Askey@gofishbc.com>
2021-11-24 12:49:46 -0800

    Proper documentation update

---
+++ R/SPDT.R
@@ -0,0 +1,13 @@
+#' SPDT
+#' 
+#' \code{SPDT} package to support SPDT data analyses. Source code at: https://github.com/PAskey/SPDT
+#' Explanation in readme file: https://github.com/PAskey/SPDT/blob/main/README.md
+
+#' Installation and Updates
+#' Install and update from PAskey github account with devtools package. 
+
+#' First install the devtools package from CRAN with:
+#' install.packages("devtools")
+
+#' Next install the SPDT package from PAskey Github repo as:
+#' devtools::install_github("PAskey/SPDT").
