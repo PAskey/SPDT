5bea76fde8be153cc25c8b9113d7ac2b1ff9fc1f
Paul Askey <Paul.Askey@gofishbc.com>
2023-05-08 15:00:29 -0700

    dropped source location a grouper in releases

--- R/linkClips.R
+++ R/linkClips.R
@@ -67,8 +67,10 @@ Releases <- Releases%>%dplyr::mutate(
 #this seems quite slow.
 Releases = Releases%>%
   mutate(Qtr = quarter(rel_Date))%>%#group by quarter in case 2 trips on different days of same fish
-  group_by(across(c(-rel_id,-rel_Date,-Quantity, -Weight, -g_size,-rel_waterbody_temp_c,-rel_waterbody_ph)))%>%
-  summarize(rel_id = paste(unique(.data$rel_id),collapse = ","), rel_Date = mean(.data$rel_Date, na.rm = TRUE), 
+  group_by(across(c(-rel_id,-rel_Date, -stock_source_loc_name, -Quantity, -Weight, -g_size,-rel_waterbody_temp_c,-rel_waterbody_ph)))%>%
+  summarize(rel_id = paste(unique(.data$rel_id),collapse = ","), 
+            rel_Date = mean(.data$rel_Date, na.rm = TRUE), 
+            stock_source_loc_name = paste(unique(.data$stock_source_loc_name),collapse = ","),
             Quantity = sum(Quantity), Weight = sum(Weight), g_size = sum(Quantity*g_size)/sum(Quantity))%>%
   select(-Qtr, ag_description)
 
